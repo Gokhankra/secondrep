8918d3d5c9fb5beb02a1464ef82dbbe64c16d38c
gokhan kara <108033505+Gokhankra@users.noreply.github.com>
2022-07-07 21:09:12 -0400

    Update News.java

--- try2/News.java
+++ try2/News.java
@@ -4,5 +4,6 @@ public class News {
     public static void main(String[] args) {
         System.out.println("line 1 in local");
         System.out.println("IT is created in github");
+        System.out.println("IT is created in githubmerge");
     }
 }
