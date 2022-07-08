9fd9691b9f0b9fff2cb7371acd80d9b0b5ac173c
gokhan kara <108033505+Gokhankra@users.noreply.github.com>
2022-07-07 23:08:00 -0400

    Update fetch.java

--- try2/fetch.java
+++ try2/fetch.java
@@ -3,6 +3,6 @@ package try2;
 public class fetch {
 // pull deyince otomatik alir ve birlestirir from remote repositry 
   // ornegin burayi pull ile
-  // eger fetch ile cekersenn git fetch ve
+  // eger fetch ile cekersenn git fetch ve update edersen projeyi
 
 }
