465dc09559f63e5ad3ec7406cbe18d52ef56c1e0
gokhan kara <108033505+Gokhankra@users.noreply.github.com>
2022-07-07 20:23:41 -0400

    Delete password directory

--- password/Class111.java
+++
@@ -1,7 +0,0 @@
-package password;
-
-public class Class111 {
-    public static void main(String[] args) {
-
-    }
-}
--- password/dontsend.java
+++
@@ -1,7 +0,0 @@
-package password;
-
-public class dontsend {
-    public static void main(String[] args) {
-
-    }
-}
