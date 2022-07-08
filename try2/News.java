cd22a73630bb3466ac219546a190fbb966d78553
gokhan kara <108033505+Gokhankra@users.noreply.github.com>
2022-07-07 23:14:10 -0400

    Update News.java

--- try2/News.java
+++ try2/News.java
@@ -8,6 +8,6 @@ public class News {
         System.out.println("new line added");
         // 1st step my branch or remote repositry is updated and i have to update my local repository or workspace
         // git pull ile bu kodu workspace e insert
-        //eger git fetch yaparsam bu line
+        //eger git fetch yaparsam bu line local repositryi update edecek yani gkhnbrnc1 i
     }
 }
